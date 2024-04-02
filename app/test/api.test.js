3382ae4cc5f6c96856d0df22ad2566714407bee2
Ben Graham <grahamben7@gmail.com>
2024-04-02 09:27:00 +0200

    Added api test command and file

---
+++ app/test/api.test.js
@@ -0,0 +1,8 @@
+
+const apiTest = async (route) => {
+    const response = await fetch(`http://127.0.0.1:5000${route}`)
+    const data = await response.json()
+    console.log(data)
+}
+
+apiTest("/insert")
