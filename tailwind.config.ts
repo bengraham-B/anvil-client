9eb21b50f1e4a242a01ffa4cd80074f2907b97b5
Ben Graham <grahamben7@gmail.com>
2024-04-09 21:36:34 +0200

    Making the Typescript version

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    screens: {
-      xsm:"270px",
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -23,3 +17,4 @@ module.exports = {
   },
   plugins: [],
 };
+export default config;
