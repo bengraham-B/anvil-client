81b867237b262d7c6f03176382d2fd812bf31164
Ben Graham <grahamben7@gmail.com>
2024-04-16 09:33:54 +0200

    User can save transaction

--- app/AddTransaction/page.tsx
+++ app/AddTransaction/page.tsx
@@ -6,6 +6,11 @@ export default function page() {
 	const [expenseClass, setExpenseClass] = useState<boolean>(false)
 	const [classValue, setClassValue] = useState<string>("")
 
+	const [amount, setAmount] = useState<string>("")
+	const [details, setDetails] = useState<string>("")
+	const [category, setCategory] = useState<string>("")
+	const [date, setDate] = useState<string>("")
+
 	const classFunc = (classType:string) => {
 		console.log(classType)
 
@@ -26,16 +31,23 @@ export default function page() {
 		const response = fetch(`http://127.0.0.1:5000/save_transaction`, {
 			method: "POST",
 			body: JSON.stringify({
-				details: ,
-				amount: ,
-				category: ,
-				class: ,
-				date: ,
+				"details": details,
+				"amount": amount,
+				"category": category,
+				"class": classValue,
+				"date": date,
 			}),
 			headers: {
 				"Content-Type": "application/json"
 			}
 		})
+
+		const data = (await response).json()
+		console.log(data)
+	}
+
+	function saveCategory(categoryValue:React.ChangeEvent<HTMLSelectElement>){
+		setCategory(categoryValue.target.value)
 	}
 
   return (
@@ -56,19 +68,20 @@ export default function page() {
 						<h3 className='text-2xl'>Amount</h3>
 						<div className='bg-white flex w-full align-middle rounded-md'>
 							<h3 className='flex pl-1 py-2 text-black align-middle text-lg text-center'>R</h3>
-							<input type="number" placeholder="0.00" className='w-full text-lg text-black pl-1 p-1 outline-none rounded-md'/>
+							<input type="number" placeholder="0.00" className='w-full text-lg text-black pl-1 p-1 outline-none rounded-md' onChange={(e) => setAmount(e.target.value)}/>
 						</div>
 					</div>
 
 					<div id="Details" className='space-y-1 py-1'>
 						<h3 className='text-2xl'>Details</h3>
-						<input type="text"  className='w-full text-lg text-black pl-2 p-1 outline-none rounded-md'/>
+						<input type="text"  className='w-full text-lg text-black pl-2 p-1 outline-none rounded-md' onChange={(e) => setDetails(e.target.value)}/>
 					</div>
 					
 					<div id="category" className='space-y-1 py-1'>
 						<h3 className='text-2xl'>Category</h3>
-						<select name="Sundry" id="" className='flex w-full text-xl bg-white text-black py-2'>
+						<select name="Sundry" id="" className='flex w-full text-xl bg-white text-black py-2' onChange={saveCategory}>
 							<option value="Sundry" className='text-lg py-2'>Sundry</option>
+							<option value="Change Category" className='text-lg py-2'>Change Category</option>
 						</select>
 					</div>
 					
@@ -77,13 +90,13 @@ export default function page() {
 						<button onClick={() => classFunc("expense")} className={`border border-red-600 py-2 px-4 rounded-md text-xl ${expenseClass ? "bg-red-600 border-green-600 outline-none text-white":"border-red-600 text-red-600 "}`}>Expense</button>
 					</div>
 
-					<div id="Details" className='space-y-2 py-1'>
+					<div id="Date" className='space-y-2 py-1'>
 						<h3 className='text-2xl'>Date</h3>
-						<input type="date"  className='w-full text-lg text-black pl-2 p-1 outline-none rounded-md'/>
+						<input type="date"  className='w-full text-lg text-black pl-2 p-1 outline-none rounded-md' onChange={(e) => setDate(e.target.value)}/>
 					</div>
 
 					<div className='flex w-full justify-center py-1'>
-						<button className="py-2 px-4 text-2xl border border-white rounded-md hover:bg-white hover:text-black">Submit</button>
+						<button className="py-2 px-4 text-2xl border border-white rounded-md hover:bg-white hover:text-black" onClick={saveTransaction}>Submit</button>
 					</div>
 					
 
